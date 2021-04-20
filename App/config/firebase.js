41e43d0c5ea63f47e834035610fc300fffe4bdf9
Lee Anderson Martins de Jesus Brasil <lee.brasil@outlook.com>
2021-04-20 08:49:52 -0300

    Update firebase.js

--- App/config/firebase.js
+++ App/config/firebase.js
@@ -8,13 +8,13 @@ import "firebase/storage";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyD1LIBmK8XKvTy8AOaY8sHTSQj77e7VsYc",
-  authDomain: "kopenhagenapp-4e26f.firebaseapp.com",
-  projectId: "kopenhagenapp-4e26f",
-  storageBucket: "kopenhagenapp-4e26f.appspot.com",
-  messagingSenderId: "1046487630212",
-  appId: "1:1046487630212:web:0d87b6943ac9a5aab29dd1",
-  measurementId: "G-JSBBYPM10Y"
+  apiKey: "xxxxxx",
+  authDomain: "xxxxxxx",
+  projectId: "xxxxxxx",
+  storageBucket: "xxxxxx",
+  messagingSenderId: "xxxxx",
+  appId: "xxxxxxxx",
+  measurementId: "xxxxxxx"
 };
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
